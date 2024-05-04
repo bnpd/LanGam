129034509b1f9b3b39f3f4760b63c69bcbd4602a
bnpd <benjamin.paddags@gmail.com>
2024-05-05 00:22:02 +0200

    remove config.js from git

--- src/config.js
+++
@@ -1,5 +0,0 @@
-export default
-{
-  "backend": "https://allai-backend.onrender.com",
-  "webpush_public_key": "BKjnf9bkM0CzP0JlcMdCuRkenz9fbgLqGrQYYZO0qTQTxHYx6k1GVG4tPu57AxyAzyljPUl7ZRL51mMZIBGGDoM"
-}
