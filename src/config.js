f03c6aed67c397a983b129fba53844b9ad325bc6
bnpd <benjamin.paddags@gmail.com>
2024-05-05 12:49:25 +0200

    re-add config.js

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default
+{
+  "backend": "https://allai-backend-1.onrender.com",
+  "webpush_public_key": "BKjnf9bkM0CzP0JlcMdCuRkenz9fbgLqGrQYYZO0qTQTxHYx6k1GVG4tPu57AxyAzyljPUl7ZRL51mMZIBGGDoM"
+}
