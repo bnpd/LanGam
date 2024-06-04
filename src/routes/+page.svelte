ba405eed9ebe849058d91495d3790409d2e3e4ab
bnpd <benjamin.paddags@gmail.com>
2024-06-05 00:39:10 +0200

    fix prerender and static site generator config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,6 @@
 <script>
   import App from '$lib/components/App.svelte';
   import './global.css';
-
-  // For static site generation (https://kit.svelte.dev/docs/adapter-static#usage):
-  export const prerender = true;
 </script>
 
 <App />
