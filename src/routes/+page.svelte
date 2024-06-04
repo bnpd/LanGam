2c2d4d1fbd99f0ff020845c4077c2a6e203d36ab
bnpd <benjamin.paddags@gmail.com>
2024-06-05 00:27:06 +0200

    add production adapter for static page generation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script>
   import App from '$lib/components/App.svelte';
   import './global.css';
+
+  // For static site generation (https://kit.svelte.dev/docs/adapter-static#usage):
+  export const prerender = true;
 </script>
 
 <App />
