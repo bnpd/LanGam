5f9bde28abc470e0952ebd49bf1156035c15b5bc
bnpd <benjamin.paddags@gmail.com>
2024-06-11 11:39:23 +0200

    nicer Toast slide-in using svelte

--- src/lib/components/Toast.svelte
+++ src/lib/components/Toast.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+	import { fly } from "svelte/transition";
+
     export let message: string | undefined = undefined;
     export let textReject: string | undefined = undefined;
     export let onReject: { (): void; } | undefined = undefined;
 
-    $: if (message) setTimeout(() => { message = undefined; }, 3000);
+    $: if (message) setTimeout(() => { message = undefined; }, 4000);
 
     function hide() {
         message=undefined
@@ -12,9 +14,11 @@
     }
 </script>
 
-<div id="toast" hidden={!message} on:touchmove={hide} on:click={hide}>
+{#if message}
+<div id="toast" on:touchmove={hide} on:click={hide} transition:fly={{ y: 100, duration: 500 }}>
     <p>{message}</p> 
     {#if onReject && textReject}
         <button on:click={onReject}>{textReject}</button>
     {/if}
-</div>
+</div>    
+{/if}
