e878fd1d1fdcabe272767efee470affbb16d5cde
bnpd <benjamin.paddags@gmail.com>
2024-07-26 20:56:44 +0200

    try falling back to google translate if langki is not installed

--- src/lib/components/TokenComponent.svelte
+++ src/lib/components/TokenComponent.svelte
@@ -1,16 +1,27 @@
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <script lang="ts">
+	import { nativeLang, targetLang } from "$lib/stores";
+
     export let word: string;
     export let isFailed: boolean;
     export let isSrWord: boolean = false;
     export let isClickable: boolean;
+
+    function onDictClick(event: Event) {
+        event.preventDefault();
+        window.open((event.currentTarget as HTMLAnchorElement).href);
+        
+        setTimeout(() => {
+            window.open(`https://translate.google.com/?sl=${$targetLang}&tl=${$nativeLang}&text=${word}`);
+        }, 250);
+    }
 </script>
 
 {#if isClickable}
 <span style:display="inline-block" class={'pointer span-'+word + (isFailed ? ' clicked' : isSrWord ? ' srWord' : '')} on:click>
     {#if isFailed}
-    <a href={'langki://word/?w='+word}>📕</a>
+    <a href={'langki://word/?w='+word} on:click={onDictClick}>📕</a>
     {/if}
     {word}
 </span>
