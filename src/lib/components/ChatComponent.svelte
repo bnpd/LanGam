f55b5ef30855157dffc77e7828f3c98d3b82c3f3
bnpd <benjamin.paddags@gmail.com>
2024-08-07 23:22:23 +0200

    show chat history and disable sending chats while loading response

--- src/lib/components/ChatComponent.svelte
+++ src/lib/components/ChatComponent.svelte
@@ -11,20 +11,20 @@
      */
     $: lastFailed = Array.from($failedWords).at(-1);
     $: lastFailedLemma = lastFailed ? findLemma(lastFailed) : undefined
-    $: if ($currentTask) response = $currentTask?.question?.text; // reset response on changes to currentTask
+    $: if ($currentTask && $currentTask?.question?.text?.length) chatHistory = [{type: 'ai', text: $currentTask.question.text}]; // reset question on changes to currentTask
     $: if (typeof document !== 'undefined') {            
             let mainContent = document?.getElementById('contentbox')
             if (mainContent) {
-                mainContent.style.opacity = (response && chatFocussed) ? "0.65" : "1"
+                mainContent.style.opacity = (chatHistory.length && chatFocussed) ? "0.65" : "1"
             }
         }
     
     export let readerComponent: ReaderComponent;
     export let chatFocussed: boolean = false;
-    let response: string | undefined;
     let chatPrompt: string | undefined
     let iChat: HTMLDivElement
     let loading: boolean = false
+    let chatHistory: {type: string, text: string}[] = [];
 
     /**
      * Finds the first occurrence of word in the text and returns the corresponding lemma (thus not necessarily the lemma for that ocurrence that the user clicked)
@@ -40,13 +40,16 @@
     }
 
     async function onClickChatSuggestion(e: Event) {
-        response = ''
         loading = true
+        chatPrompt = (e.currentTarget as HTMLButtonElement).innerText
         try {
-            response = $user ? await sendChat((e.currentTarget as HTMLButtonElement).innerText, undefined, undefined, readerComponent.getVisibleParagraphs())
-                         : ANON_RESPONSE
+            let response = {type: 'ai', text: ($user ? await sendChat(chatPrompt, undefined, undefined, readerComponent.getVisibleParagraphs())
+                         : ANON_RESPONSE)};
+            chatHistory.push({type: 'user', text: chatPrompt})
+            chatHistory.push(response)
+            chatPrompt = ''
         } catch (e) {
-            response = 'Cannot connect, please try again'
+            chatHistory.push({type: 'system', text: 'Cannot connect, please try again'})
         } finally {
             loading = false
         }
@@ -54,13 +57,15 @@
 
     async function onSubmitChatPrompt(e: Event) {
         if (chatPrompt) {
-            response = ''
             loading = true
             try {
-                response = $user ? await sendChat(chatPrompt, $targetLang, $currentTask.docId, undefined)
-                                 : ANON_RESPONSE
+                let response = {type: 'ai', text: ($user ? await sendChat(chatPrompt, $targetLang, $currentTask.docId, undefined)
+                                 : ANON_RESPONSE)};
+                chatHistory.push({type: 'user', text: chatPrompt})
+                chatHistory.push(response)
+                chatPrompt = ''
             } catch (e) {
-                response = 'Cannot connect, please try again'
+                chatHistory.push({type: 'system', text: 'Cannot connect, please try again'})
             } finally {
                 loading = false
             }
@@ -71,28 +76,43 @@
 </script>
 
 <div id="chatComponent" on:focus|capture={()=>{chatFocussed = true}} on:focusout|capture={()=>{chatFocussed = false}}>
-    {#if response && chatFocussed || loading}
-        <div class="card" class:loading style="max-height: 18vh; overflow-y: scroll;" id="responseBox">
-            <em><strong><BadgeComponent text='AI' tooltip="AI's response"/></strong></em>&nbsp;{response}
-        </div>
+    {#if chatHistory.length && chatFocussed || loading}
+        {#each chatHistory as msg, i}
+                {#if msg.type === 'ai'}
+                    <div class="card" class:loading style="max-height: 18vh; overflow-y: scroll;" id="responseBox">
+                        <em><strong><BadgeComponent text='AI' tooltip="AI's response"/></strong></em>&nbsp;
+                        {msg.text}
+                    </div>
+                {:else if msg.type === 'user'}
+                    <div class="card" class:loading style="max-height: 18vh; overflow-y: scroll;" id="responseBox">
+                        <em><strong><BadgeComponent text='You' tooltip="Your response"/></strong></em>&nbsp;
+                        {msg.text}
+                    </div>
+                {:else if msg.type === 'system' && i == chatHistory.length-1}
+                    <div class="card" class:loading style="max-height: 18vh; overflow-y: scroll;" id="responseBox">
+                        <em><strong><BadgeComponent text='Error' tooltip="Error"/></strong></em>&nbsp;
+                        {msg.text}
+                    </div>
+                {/if}
+        {/each}
     {/if}
     <div>
-        <button class="promptSuggestion" on:click={onClickChatSuggestion}>
+        <button class="promptSuggestion" on:click={onClickChatSuggestion} disabled={loading}>
             How is the past tense formed?
         </button>
         {#if lastFailed}
             {#if lastFailedLemma !== lastFailed}
-                <button class="promptSuggestion" on:click={onClickChatSuggestion}>
+                <button class="promptSuggestion" on:click={onClickChatSuggestion} disabled={loading}>
                     Why is it {lastFailed} and not {lastFailedLemma} here?
                 </button>
             {/if}
-            <button class="promptSuggestion" on:click={onClickChatSuggestion}>
+            <button class="promptSuggestion" on:click={onClickChatSuggestion} disabled={loading}>
                 Why is {lastFailed} used here?
             </button>
         {/if}
     </div>
     <div contenteditable id="iChat" data-placeholder="Ask me ✨" bind:textContent={chatPrompt} bind:this={iChat}/>
-    <button id="submit" on:click={onSubmitChatPrompt}><b><em>
+    <button id="submit" on:click={onSubmitChatPrompt} disabled={loading}><b><em>
         {#if chatPrompt}
         ➥
         {:else}
