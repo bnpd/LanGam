d2c6445da3bde910bd5f9be8118d433851e8d379
bnpd <benjamin.paddags@gmail.com>
2024-08-13 12:06:26 +0200

    avif format for background image

--- src/lib/components/TitleWithBackgroundImageComponent.svelte
+++ src/lib/components/TitleWithBackgroundImageComponent.svelte
@@ -22,7 +22,7 @@
       /* Gradient overlay for fade effect as top layer, image as bottom layer */
       background: 
         linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, var(--body-background-color-half-opaque) 66%, var(--body-background-color) 100%),
-        url('/images/Polish.png') no-repeat center center;
+        url('/images/Polish.avif') no-repeat center center;
       background-size: cover, cover; /* Apply cover to both layers */
       background-position: middle; /* Set the desired position */
     }
