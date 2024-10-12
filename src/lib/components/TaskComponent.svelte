1a9743cb885bec51c3fbf10b08a9056fb0b71b73
bnpd <benjamin.paddags@gmail.com>
2024-10-12 13:05:17 +0200

    doc.title is displayed as h3 if it does not contain #s for heading level specification

--- src/lib/components/TaskComponent.svelte
+++ src/lib/components/TaskComponent.svelte
@@ -15,35 +15,38 @@
     function makeTask(doc: DocumentC) {
         let resParas = []
         let space = {word: ' ', lemma_: '', pos: -1}
+        let isTitle = true
         for (const translatableText of [doc.title, doc.text]) {
-            if (!translatableText) continue
-            let paragraph = []	
-            let char_index = 0	
-            for (const start_char in translatableText.tokens) {                       
-                while (char_index < (start_char as unknown as number)) { // insert whitespace and newlines
-                if (translatableText.text[char_index] == '\n' && paragraph.length > 0) {
-                    // next paragraph
+            if (translatableText) {
+                let paragraph = []	
+                let char_index = 0	
+                for (const start_char in translatableText.tokens) {                       
+                    while (char_index < (start_char as unknown as number)) { // insert whitespace and newlines
+                    if (translatableText.text[char_index] == '\n' && paragraph.length > 0) {
+                        // next paragraph
+                        const headingLevel = getHeadingLevelForTask(paragraph)
+                        resParas.push({htmlTag: isTitle && !headingLevel ? 'h3' : headingLevel ? 'h'+headingLevel : 'p', words: paragraph.slice(headingLevel)})
+                        paragraph = []
+                    } else if (paragraph.length > 0) { // length>0 cause we do not want to push leading spaces to paragraphs
+                        paragraph.push(space)
+                    }
+                    char_index++
+                    }
+                    // now our char indexes are synced
+    
+                    let token_obj = translatableText.tokens[char_index]
+                    let token_word = token_obj?.word
+    
+                    if (!token_word?.trim()) continue // only proceed if it wasn't only some kind of whitespace
+                    paragraph.push(token_obj)
+                    char_index += token_word.length
+                }
+                if(paragraph.length > 0) {
                     const headingLevel = getHeadingLevelForTask(paragraph)
-                    resParas.push({htmlTag: headingLevel ? 'h'+headingLevel : 'p', words: paragraph.slice(headingLevel)})
-                    paragraph = []
-                } else if (paragraph.length > 0) { // length>0 cause we do not want to push leading spaces to paragraphs
-                    paragraph.push(space)
+                    resParas.push({htmlTag: isTitle && !headingLevel ? 'h3' : headingLevel ? 'h'+headingLevel : 'p', words: paragraph.slice(headingLevel)})
                 }
-                char_index++
-                }
-                // now our char indexes are synced
-
-                let token_obj = translatableText.tokens[char_index]
-                let token_word = token_obj?.word
-
-                if (!token_word?.trim()) continue // only proceed if it wasn't only some kind of whitespace
-                paragraph.push(token_obj)
-                char_index += token_word.length
             }
-            if(paragraph.length > 0) {
-                const headingLevel = getHeadingLevelForTask(paragraph)
-                resParas.push({htmlTag: headingLevel ? 'h'+headingLevel : 'p', words: paragraph.slice(headingLevel)})
-            }
+            isTitle = false // done with title
         }
         return resParas
     }
