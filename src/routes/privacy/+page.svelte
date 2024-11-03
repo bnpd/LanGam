d4fc29dca79c22c6257ef47f76578479dfd8bef8
bnpd <benjamin.paddags@gmail.com>
2024-11-03 10:39:25 +0100

    remove broken and basecamp links from privacy policy

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -17,50 +17,50 @@
   </ul>
   <p dir="auto">This policy applies to all products built and maintained by Benjamin Paddags (for the sake of simplicity referred to as "we") including LanGam.</p>
   <p dir="auto">This policy applies to our handling of information about site visitors, prospective customers, and customers and authorized users (in relation to their procurement of the services and management of their relationship with us). We refer collectively to these categories of individuals as "you" throughout this policy.</p>
-  <p dir="auto">If you are a California resident, please&nbsp;<a href="/ccpa">click here to see our California Notice at Collection</a>, which includes additional disclosures as required by California law.</p>
+  <!--<p dir="auto">If you are a California resident, please&nbsp;<a href="/ccpa">click here to see our California Notice at Collection</a>, which includes additional disclosures as required by California law.</p>-->
   <div dir="auto">
   <h2 dir="auto" tabindex="-1">What we collect and why</h2>
-  <a id="user-content-what-we-collect-and-why" href="https://github.com/basecamp/policies/blob/master/privacy/index.md#what-we-collect-and-why" aria-label="Permalink: What we collect and why"></a></div>
+  </div>
   <p dir="auto">Our guiding principle is to collect only what we need. Here&rsquo;s what that means in practice:</p>
   <div dir="auto">
   <h3 dir="auto" tabindex="-1">Identity and access</h3>
-  <a id="user-content-identity-and-access" href="https://github.com/basecamp/policies/blob/master/privacy/index.md#identity-and-access" aria-label="Permalink: Identity and access"></a></div>
+  </div>
   <p dir="auto">When you sign up for a Benjamin Paddags product, we ask for identifying information such as your name and email address. This allows you to recover your account using your email address if you forget your password, personalize your new account, and receive essential product updates (Legal basis: Contractual Necessity). We may also send you optional surveys to understand how you use our products and to make improvements (Legal basis: Legitimate Interest). With your consent, we will send you our newsletter and other updates (Legal basis: Consent). We sometimes also give you the option to add a profile picture that displays in our products (Legal basis: Consent).</p>
   <p dir="auto">We&rsquo;ll never sell your personal information to third parties, and we won&rsquo;t use your name in marketing statements without your permission either.</p>
   <div dir="auto">
   <h3 dir="auto" tabindex="-1">Billing information</h3>
-  <a id="user-content-billing-information" href="https://github.com/basecamp/policies/blob/master/privacy/index.md#billing-information" aria-label="Permalink: Billing information"></a></div>
+  </div>
   <p>If you sign up for a paid Benjamin Paddags product, you will be asked to provide payment information and a billing address. Credit card information is submitted directly to our payment processor and does not reach Benjamin Paddags servers. We store a record of the payment transaction, including the last 4 digits of the credit card number, to maintain account history, invoicing, and billing support (Legal basis: Contractual Necessity and Legal Obligation). We store your billing address for invoicing, sales tax calculation, and fraud detection (Legal basis: Legal Obligation). We occasionally use aggregate billing information to guide marketing efforts (Legal basis: Legitimate Interest).</p>
   <div dir="auto">
   <h3 dir="auto" tabindex="-1">Product interactions</h3>
-  <a id="user-content-product-interactions" href="https://github.com/basecamp/policies/blob/master/privacy/index.md#product-interactions" aria-label="Permalink: Product interactions"></a></div>
+  </div>
   <p>We store data necessary for you to use our products, such as the languages you learn in LanGam and your vocabulary study history, games started, and in-game progress. This data is kept as long as your account is active (Legal basis: Contractual Necessity). If you delete your account, we&rsquo;ll delete this content within 60 days.</p>
   <div dir="auto">
   <h3 dir="auto" tabindex="-1">General Geolocation data</h3>
-  <a id="user-content-general-geolocation-data" href="https://github.com/basecamp/policies/blob/master/privacy/index.md#general-geolocation-data" aria-label="Permalink: General Geolocation data"></a></div>
+  </div>
   <p>For most of our products, we log the IP address used to sign up for an account to mitigate spam signups (Legal basis: Legitimate Interest). We also log all account access by IP address for security and fraud prevention purposes, retaining this login data as long as your account is active (Legal basis: Legitimate Interest).</p>
   <div dir="auto">
   <h3 dir="auto" tabindex="-1">Website interactions</h3>
-  <a id="user-content-website-interactions" href="https://github.com/basecamp/policies/blob/master/privacy/index.md#website-interactions" aria-label="Permalink: Website interactions"></a></div>
+  </div>
   <p dir="auto">We collect information about your browsing activity for analytics and statistical purposes such as conversion rate testing and experimenting with new product designs. This includes, for example, your browser and operating system versions, your IP address, which web pages you visited and how long they took to load, and which website referred you to us. If you have an account and are signed in, these web analytics data are tied to your IP address and user account until your account is no longer active &nbsp;(Legal basis: Legitimate Interest).</p>
   <div dir="auto">
   <h3 dir="auto" tabindex="-1">Anti-bot assessments</h3>
-  <a id="user-content-anti-bot-assessments" href="https://github.com/basecamp/policies/blob/master/privacy/index.md#anti-bot-assessments" aria-label="Permalink: Anti-bot assessments"></a></div>
+  </div>
   <p dir="auto">We use CAPTCHA across our applications to mitigate brute force logins and prevent spam. CAPTCHA assesses various information, like IP address and mouse movements, to detect non-human activity (Legal basis: Legitimate Interest).</p>
   <div dir="auto">
   <h3 dir="auto" tabindex="-1">Advertising and Cookies</h3>
-  <a id="user-content-advertising-and-cookies" href="https://github.com/basecamp/policies/blob/master/privacy/index.md#advertising-and-cookies" aria-label="Permalink: Advertising and Cookies"></a></div>
+  </div>
   <p dir="auto">Benjamin Paddags runs contextual ads on various third-party platforms such as Google, Reddit, and LinkedIn. Users who click on one of our ads will be sent to the LanGam marketing site. Where permissible under law, we may load an ad-company script on their browsers that sets a third-party cookie and sends information to the ad network to enable evaluation of the effectiveness of our ads, e.g., which ad they clicked and which keyword triggered the ad, and whether they performed certain actions such as clicking a button or submitting a form &nbsp;(Legal basis: Legitimate Interest).</p>
   <p dir="auto">We also use persistent first-party cookies and some third-party cookies to store certain preferences, make it easier for you to use our applications, and perform A/B testing as well as support some analytics &nbsp;(Legal basis: Consent for non-essential cookies).</p>
   <p dir="auto">A cookie is a piece of text stored by your browser. It may help remember login information and site preferences. It might also collect information such as your browser type, operating system, web pages visited, duration of visit, content viewed, and other click-stream data. You can adjust cookie retention settings and accept or block individual cookies in your browser settings, although our apps won&rsquo;t work and other aspects of our service may not function properly if you turn cookies off. Where we use third party cookies and tracking, we show you a cookie consent banner at your first visit, where you can adjust your preferences.</p>
   <div dir="auto">
   <h3 dir="auto" tabindex="-1">Voluntary correspondence</h3>
-  <a id="user-content-voluntary-correspondence" href="https://github.com/basecamp/policies/blob/master/privacy/index.md#voluntary-correspondence" aria-label="Permalink: Voluntary correspondence"></a></div>
+  </div>
   <p dir="auto">When you email Benjamin Paddags with a question or to ask for help, we keep that correspondence, including your email address, so that we have a history of past correspondence to reference if you reach out in the future&nbsp;(Legal basis: Legitimate Interest).</p>
   <p dir="auto">We also store information you may volunteer, for example, written feedback or responses to surveys&nbsp;(Legal basis: Consent).</p>
   <div dir="auto">
   <h2 dir="auto" tabindex="-1">When we access or disclose your information</h2>
-  <a id="user-content-when-we-access-or-disclose-your-information" href="https://github.com/basecamp/policies/blob/master/privacy/index.md#when-we-access-or-disclose-your-information" aria-label="Permalink: When we access or disclose your information"></a></div>
+  </div>
   <p dir="auto"><strong>To provide products or services you&rsquo;ve requested</strong>. We use some third-party subprocessors to help run our applications and provide the Services to you. We establish GDPR-compliant data processing agreements with each subprocessor, extending&nbsp;<a href="/privacy">GDPR safeguards</a> everywhere personal data is processed. We use the following third-party subprocessors for LanGam:</p>
   <ul dir="auto">
   <li><a href="https://www.digitalocean.com/legal/gdpr">DigitalOcean</a>. Cloud services provider. You data is kept on servers in the EU.</li>
@@ -82,7 +82,7 @@
   <p dir="auto">Finally, if Benjamin Paddags is acquired by or merges with another company &mdash; we don&rsquo;t plan on that, but if it happens &mdash; we&rsquo;ll notify you well before any of your personal information is transferred or becomes subject to a different privacy policy.</p>
   <div dir="auto">
   <h2 dir="auto" tabindex="-1">Your rights with respect to your information</h2>
-  <a id="user-content-your-rights-with-respect-to-your-information" href="https://github.com/basecamp/policies/blob/master/privacy/index.md#your-rights-with-respect-to-your-information" aria-label="Permalink: Your rights with respect to your information"></a></div>
+  </div>
   <p dir="auto">At Benjamin Paddags, we strive to apply the same data rights to all customers, regardless of their location. Some of these rights include:</p>
   <ul dir="auto">
   <li><strong>Right to Know.</strong>&nbsp;You have the right to know what personal information is collected, used, shared or sold. We outline both the categories and specific bits of data we collect, as well as how they are used, in this privacy policy.</li>
@@ -101,19 +101,19 @@
   <p dir="auto">Depending on applicable law, you may have the right to appeal our decision to deny your request, if applicable. We will provide information about how to exercise that right in our response denying the request. You also have the right to lodge a complaint with a supervisory authority. If you are in the EU or UK, you can contact your data protection authority to file a complaint or learn more about local privacy laws.</p>
   <div dir="auto">
   <h2 dir="auto" tabindex="-1">How we secure your data</h2>
-  <a id="user-content-how-we-secure-your-data" href="https://github.com/basecamp/policies/blob/master/privacy/index.md#how-we-secure-your-data" aria-label="Permalink: How we secure your data"></a></div>
+  </div>
   <p dir="auto">All data is encrypted via&nbsp;<a href="https://en.wikipedia.org/wiki/Transport_Layer_Security" rel="nofollow">SSL/TLS</a> when transmitted from our servers to your browser. The database backups are also encrypted. Most data are not encrypted while they live in our database (since they need to be ready to send to you when you need them). To secure your data at rest, we use role-based access controls to limit who can access different types of data. We use secure authentication measures, such as unique logins and strong passwords for accessing databases and administrative dashboards. We periodically review and update our security practices.</p>
   <div dir="auto">
   <h2 dir="auto" tabindex="-1">Data retention</h2>
-  <a id="user-content-data-retention" href="https://github.com/basecamp/policies/blob/master/privacy/index.md#data-retention" aria-label="Permalink: Data retention"></a></div>
+  </div>
   <p dir="auto">We keep your information for the time necessary for the purposes for which it is processed (for most data, this is until you delete your account). The length of time for which we retain information depends on the purposes for which we collected and use it. We may also retain and use this information as necessary to comply with our legal obligations, resolve disputes, and enforce our agreements.</p>
   <div dir="auto">
   <h2 dir="auto" tabindex="-1">Location of site and data</h2>
-  <a id="user-content-location-of-site-and-data" href="https://github.com/basecamp/policies/blob/master/privacy/index.md#location-of-site-and-data" aria-label="Permalink: Location of site and data"></a></div>
+  </div>
   <p dir="auto">Our products and other web properties are operated in the EU and no personal data is transferred to third party servers outside of the EU.</p>
   <div dir="auto">
   <h2 dir="auto" tabindex="-1">Changes and questions</h2>
-  <a id="user-content-changes-and-questions" href="https://github.com/basecamp/policies/blob/master/privacy/index.md#changes-and-questions" aria-label="Permalink: Changes and questions"></a></div>
+  </div>
   <p dir="auto">We may update this policy as needed to comply with relevant regulations and reflect any new practices.&nbsp;Whenever we make a significant change to our policies, we will refresh the date at the top of this page and take any other appropriate steps to notify users.</p>
   <p dir="auto">Have any questions, comments, or concerns about this privacy policy, your data, or your rights with respect to your information? Please get in touch by emailing us at&nbsp;<a href="mailto:privacy@langam.app">privacy@langam.app</a> and we&rsquo;ll be happy to try to answer them!</p>
   <p dir="auto">---</p>
