4571d907cef59c67bef000ea5ac20eb8de2f2449
bnpd <benjamin.paddags@gmail.com>
2024-11-09 00:52:47 +0100

    formComponent can handle checkboxes and validates the input json for duplicate ids etc

--- src/lib/components/FormComponent.svelte
+++ src/lib/components/FormComponent.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
-    export let fields: {id: string, name: string, value?: string, hidden?: string}[]
+    export let fields: {id: string, name: string, type?: string, value?: string, hidden?: boolean, checked?: boolean}[]
     export let submitOptions: {text: string, handler: (formdata: { [x: string]: string | undefined; }, disableOnSubmit?: boolean, disabled?: boolean, cssClass?: string) => void}
 
-    let divFieldsById: {[id: string]: HTMLDivElement} = {}
+    const ALLOWED_TYPES = ['text', 'checkbox']
+
+    $: if (new Set(fields.map(field=>field.id)).size != fields.length) {
+      console.error('Duplicate input ids used for form');
+    } else if (fields.some(field => field.type && !ALLOWED_TYPES.includes(field.type))) {
+      console.error('Unknown input type used for form');
+    } else if (fields.some(field => field.type == 'checkbox' && field.value != undefined)) {
+      console.error('Checkboxes should use checked instead of value');
+    }
+
+    let inputsById: {[id: string]: HTMLElement} = {}
 
     function onFocusNode(e: Event) {
       // re-activate all submit buttons
@@ -27,17 +37,25 @@
   margin-bottom: 0.5em;
 }
 
+.input-container:has(> input[type="checkbox"]) {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
 label {
+  font-size: x-small; 
+  pointer-events: none; 
+  font-style: italic;
+}
+
+label:has(+ div[contenteditable]) {
   position: absolute;
   top: -0.5em; 
   left: 0; 
   background-color: aliceblue; 
   padding: 0.4em 1.6em 0.4em 1.5em; 
   border-radius: 1em 1em 1em 0;
-  font-size: x-small; 
-  pointer-events: none; 
-  font-style: italic;
-  
 }
 
 div[contenteditable] {
@@ -52,8 +70,13 @@ div[contenteditable] {
     <form>
         {#each fields as field}
         <div class="input-container">
+          {#if !field.type || field.type === 'text'}
             <label for={field.id} hidden={field.hidden}>{field.name}</label>
-            <div contenteditable id={field.id} hidden={field.hidden} bind:this={divFieldsById[field.id]} on:focus={onFocusNode}>{field?.value ?? ''}</div>
+            <div contenteditable id={field.id} hidden={field.hidden} bind:this={inputsById[field.id]} on:focus={onFocusNode}>{field?.value ?? ''}</div>
+          {:else if field.type === 'checkbox'}
+            <input type="checkbox" id={field.id} hidden={field.hidden} bind:this={inputsById[field.id]} on:focus={onFocusNode} checked={field?.checked ?? false}>&nbsp;
+            <label for={field.id} hidden={field.hidden}>{field.name}</label>
+          {/if}
         </div>
         {/each}
         {#each submitOptions as submitOption}
@@ -61,7 +84,7 @@ div[contenteditable] {
             type="submit" 
             value={submitOption.text} 
             on:click|preventDefault={()=>{
-              submitOption.handler(fields.reduce((acc, field) => {acc[field.id] = divFieldsById[field.id]?.innerText; return acc}, {}))
+              submitOption.handler(fields.reduce((acc, field) => {acc[field.id] = inputsById[field.id] instanceof HTMLDivElement ? inputsById[field.id]?.innerText : inputsById[field.id].type === 'checkbox' ? inputsById[field.id]?.checked : new Error('this type should not be here'); return acc}, {}))
               if (submitOption.disableOnSubmit) submitOption.disabled = true
             }}
             disabled={submitOption.disabled}
