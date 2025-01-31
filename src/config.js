6420892a66dc98dbd312697a783ba2192e7b15c5
bnpd <benjamin.paddags@gmail.com>
2025-01-31 16:09:11 +0100

    undo master->svelte merge

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default
 {
-  "backend": "https://pocket.langam.app/",
-  "frontend": "https://langam.app",
-  "pocketbase": "https://pocket.langam.app/",
+  "backend": "https://allai.pockethost.io",
+  "frontend": "https://allai-frontend.onrender.com",
+  "pocketbase": "https://allai.pockethost.io/",
   "webpush_public_key": "BKjnf9bkM0CzP0JlcMdCuRkenz9fbgLqGrQYYZO0qTQTxHYx6k1GVG4tPu57AxyAzyljPUl7ZRL51mMZIBGGDoM"
 }
