6582d6713a64b3cb250e90fb3db176975545cc67
bnpd <benjamin.paddags@gmail.com>
2025-02-05 23:45:53 +0100

    start with smaller bg-image until task is loaded

--- src/lib/components/TitleWithBackgroundImageComponent.svelte
+++ src/lib/components/TitleWithBackgroundImageComponent.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+	import { currentTask } from "$lib/stores";
+
   export let height: string | undefined = undefined
 </script>
 
-<style>
+<style id="header-style">
     .header {
       position: relative;
       width: 100%;
@@ -15,7 +17,7 @@
       color: aliceblue;
     }
     
-    /* Background image */
+    /* Background image, starts with small version in ::after element, then big version is layed on top */
     .header::before {
       content: '';
       position: fixed;
@@ -24,16 +26,38 @@
       width: 100%;
       height: var(--header-before-height, 10dvh); /* Matches the height of the header */
       z-index: -1; /* Ensure it is behind the title */
+
+      /* Transition to higher quality version */
+      opacity: 0;
+      transition: opacity 0.2s ease-in-out 1s; /* 1s fade-in after a 1s delay so that it has time to load */
+    }
+
+    .header.lazy-image::before {
+        background: 
+          linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, var(--body-background-color-half-opaque) 66%, var(--body-background-color) 100%),
+          url('/images/Polish.avif') no-repeat center center;
+        background-size: cover, cover; /* Apply cover to both layers */
+        opacity: 1;
+    }
+
+    .header::after {
+      content: "";
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: var(--header-before-height, 10dvh); /* Matches the height of the header */
+      z-index: -2; /* Ensure it is behind the title */
       /* Gradient overlay for fade effect as top layer, image as bottom layer */
       background: 
         linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, var(--body-background-color-half-opaque) 66%, var(--body-background-color) 100%),
-        url('/images/Polish.avif') no-repeat center center;
+        url('/images/Polish-small.avif') no-repeat center center;
       background-size: cover, cover; /* Apply cover to both layers */
       background-position: middle; /* Set the desired position */
     }
 </style>
 
-<div class="header" style={height ? `--header-before-height: ${height}` : ''}>
+<div class="header" class:lazy-image={$currentTask} style={height ? `--header-before-height: ${height}` : ''}>
   <h1 class="title">
     <slot />
   </h1>
