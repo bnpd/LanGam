f48e257c0441e11bd1e63b671ab323009b20b23a
Ben <ben@langam.app>
2025-07-28 18:25:07 +0200

    add pockethost to connect-src

--- svelte.config.js
+++ svelte.config.js
@@ -30,7 +30,7 @@ const config = {
 		csp: {
 			directives: {
 				'default-src': ['self', 'https://*.langam.app'],
-				'connect-src': ['self', 'https://*.langam.app', 'https://*.wiktionary.org'],
+				'connect-src': ['self', 'https://*.langam.app', 'https://langam.pockethost.io', 'https://*.wiktionary.org'],
 				'script-src': ['self', 'https://*.langam.app'],
 				'media-src': ['self', 'data:', 'https:'],
 				'img-src': ['self', 'data:', 'https:'],
