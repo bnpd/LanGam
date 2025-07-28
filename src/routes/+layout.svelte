67559920a14ad988bafc6f71f3a2c43afb409bcd
Ben <ben@langam.app>
2025-07-28 18:05:14 +0200

    move umami to +layout.svelte and use .env vars

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { PUBLIC_UMAMI_URL } from '$env/static/public';
+	import { PUBLIC_UMAMI_WEBSITE_ID } from '$env/static/public';
+	import { PUBLIC_UMAMI_SCRIPT_INTEGRITY } from '$env/static/public';
+</script>
+
+<svelte:head>
+	<script async defer src={`${PUBLIC_UMAMI_URL}/script.js`} data-website-id={PUBLIC_UMAMI_WEBSITE_ID} integrity={PUBLIC_UMAMI_SCRIPT_INTEGRITY} crossorigin="anonymous"></script>
+</svelte:head>
